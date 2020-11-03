c2c9b94fb961293d87093ed5dd99de296c6880fc
T007 <1255965856@qq.com>
2020-11-03 22:46:58 +0800

    首页交互——列表显示、上拉加载、下拉刷新

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,134 +1,97 @@
 <template>
 	<view>
-		<!-- <view>
-			<image src="../../static/tabbar/1.jpg" class="img" style="width: 750rpx;height: 300rpx;"></image>
-		</view> -->
-		
-		<!-- 轮播图 -->
-		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="200" style="width: 750rpx;height: 250rpx;">
-			<swiper-item>
-				<image src="../../static/tabbar/1.jpg" style="width: 750rpx;height: 250rpx;"></image>
-			</swiper-item>
-			<swiper-item>
-				<image src="../../static/tabbar/2.jpg" style="width: 750rpx;height: 250rpx;"></image>
-			</swiper-item>
-		</swiper>
-		
+		<view class="top flex align-center justify-center">
+			<input style="width: 600rpx;height: 70rpx; background-color: rgba(0,0,0,0.2);" type="text" class="rounded-circle mx-1 pl-5"
+			 placeholder="搜索直播间" />
+		</view>
+
 		<!-- 列表 -->
 		<view class="flex flex-wrap">
-			
-			<!-- <view class="list-item" v-for="(item, index) in list" :key="index" @click="openLive">
-				<image :src="item.url" style="width: 360rpx;height: 360rpx;" class="rounded" mode="aspectFill"></image>
-				<view class="rounded-circle px-2 flex align-center" style="position: absolute;left: 15rpx;top: 15rpx;background-color: rgba(0,0,0,0.4);">
-					<text class="iconfont iconbizhongguanli text-warning mr-1"></text>
-					<text class="text-white font">{{ item.gold }}</text>
-				</view>
-				
-				<view class="rounded-circle px-2 flex align-center" style="position: absolute;right: 15rpx;top: 15rpx;background-color: rgba(0,0,0,0.4);">
-					<text class="iconfont iconbizhongguanli text-warning mr-1"></text>
-					<text class="font-sm text-white">人气：</text>
-					<text class="text-white font-sm">{{ item.popularity }}</text>
-				</view>
-				
-				<view class="rounded-circle flex align-center" style="position: absolute;left: 15rpx;bottom: 15rpx;">
-					<text class="text-white font">{{ item.title }}</text>
-				</view>
-				
-				<view class="rounded-circle px-2 flex align-center" style="position: absolute;right: 15rpx;bottom: 15rpx;background-color: rgba(0,0,0,0.4);">
-					<text style="width: 20rpx;height: 20rpx;" class="rounded-circle bg-danger mr-1"></text>
-					<text v-if="item.status" class="text-white font-sm">已结束</text>
-					<text v-else class="text-white font-sm">已结束</text>
-				</view>
-			</view> -->
-			
-			<f-list
-				class="list-item"
-				v-for="(item, index) in list"
-				:key="index"
-				:item="item"
-				:index="index"
-				@click="openLive">
-			</f-list>
+			<view class="list-item" v-for="(item, index) in list" :key="index">
+				<f-card :item="item" :index="index" @click="openLive(item.id)"></f-card>
+			</view>
+		</view>
+
+		<view class="f-divider"></view>
+		<view class="flex align-center justify-center py-3">
+			<text class="font-md text-secondary">{{ loadText }}</text>
 		</view>
 	</view>
-	
 </template>
 
 <script>
-	// 引用封装的组件
-	import fList from "../../components/common/f-list.vue"
-	
+	import fCard from '@/components/common/f-card.vue';
 	export default {
 		components: {
-			fList
+			fCard
 		},
 		data() {
 			return {
-				list: [
-					{
-						coin: 10,
-						hoy: 10,
-						title: "LOL",
-						status: false,
-						cover: "/static/beauty/me.jpg"
-					},
-					{
-						coin: 11,
-						hoy: 10,
-						title: "LOL",
-						status: false,
-						cover: "/static/beauty/m1.jpg"
-					},
-					{
-						coin: 21,
-						hoy: 10,
-						title: "LOL",
-						status: false,
-						cover: "/static/beauty/m2.jpg"
-					},
-					{
-						coin: 13,
-						hoy: 10,
-						title: "LOL",
-						status: false,
-						cover: "/static/beauty/m3.jpg"
-					},
-					{
-						coin: 41,
-						hoy: 10,
-						title: "LOL",
-						status: false,
-						cover: "/static/beauty/m4.jpg"
-					},
-					{
-						coin: 51,
-						hoy: 10,
-						title: "LOL",
-						status: false,
-						cover: "/static/beauty/m5.jpg"
-					},
-					{
-						coin: 21,
-						hoy: 10,
-						title: "LOL",
-						status: false,
-						cover: "/static/banner/XGY1.jpg"
-					}
-				]
-			}
+				list: [],
+				page: 1,
+				loadText: '上拉加载更多'
+			};
 		},
-		onLoad() {},
+		onLoad() {
+			this.getData();
+		},
+		onPullDownRefresh() {
+			this.page = 1;
+			this.getData()
+				.then(res => {
+					uni.showToast({
+						title: '刷新成功',
+						icon: 'none'
+					});
+					uni.stopPullDownRefresh();
+				})
+				.catch(err => {
+					uni.stopPullDownRefresh();
+				});
+		},
+		onReachBottom() {
+			if (this.loadText != '上拉加载更多') {
+				return;
+			}
+			this.loadText = '加载中...';
+			this.page++;
+			this.getData();
+		},
 		methods: {
-			openLive() {
+			getData() {
+				let page = this.page;
+				return this.$H
+					.get('/live/list/' + page)
+					.then(res => {
+						(this.list = page === 1 ? res : [...this.list, ...res]),
+						(this.loadText = res.length < 10 ? '没有更多了' : '上拉加载更多');
+					})
+					.catch(err => {
+						if (this.page > 1) {
+							this.page--;
+							this.loadText = '上拉加载更多';
+						}
+					});
+			},
+			openLive(e) {
+				console.log(e);
 				uni.navigateTo({
-					url: '../live/live'
+					url: '../live/live?id=' + e
 				});
 			}
 		}
-	}
+	};
 </script>
 
 <style>
+	.top {
+		width: 750rpx;
+		height: 260rpx;
+		background-image: url(../../static/banner/3.jpg);
+		background-size: cover;
+		background-image: linear-gradient(to right, #ba7ace 0%, #8745ff 100%);
+	}
+
 	.list-item {
 		width: 375rpx;
 		height: 375rpx;
